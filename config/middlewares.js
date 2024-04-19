dd5ed49dbdfdac38851f41d3f2c53d483598cd93
Rian Tavares <riantavares@192.168.1.6>
2024-04-19 14:07:36 -0700

    setup s3 bucket for deal with images

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,32 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'zero1-aws-s3-images-bucket.s3.sa-east-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'zero1-aws-s3-images-bucket.s3.sa-east-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
